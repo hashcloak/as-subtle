dc85eae8c3376ee0a54b2352dab8b7d55afcde55
Mikerah <mikerahqc@protonmail.com>
2019-10-24 21:49:20 -0400

    Added constant time selection for i64 type

--- assembly/index.ts
+++ assembly/index.ts
@@ -24,6 +24,14 @@ export function constSelect32(a: i32, b: i32, c: i32 ): i32 {
     
 }
 
+export function constSelect64(a: i64, b: i64, c: i64 ): i32 {
+    if (a < 0 || a > 1) {
+        throw new Error("a needs to be either 0 or 1");
+    }
+
+    return !(a-1) & b | (a-1) & y;
+    
+}
 export function constEq32(): i32 {
 
 }
