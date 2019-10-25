367171afcafbdea32c2740a9a1aad74916a8173f
Mikerah <mikerahqc@protonmail.com>
2019-10-24 21:48:33 -0400

    Added constant time selection for i32 type

--- assembly/index.ts
+++ assembly/index.ts
@@ -15,8 +15,13 @@ export function constArrayCompare(a: i32[], b: i32[]): i32 {
     return constEq32(c, 0);
 }
 
-export function constSelect(): i32 {
+export function constSelect32(a: i32, b: i32, c: i32 ): i32 {
+    if (a < 0 || a > 1) {
+        throw new Error("a needs to be either 0 or 1");
+    }
 
+    return !(a-1) & b | (a-1) & y;
+    
 }
 
 export function constEq32(): i32 {
